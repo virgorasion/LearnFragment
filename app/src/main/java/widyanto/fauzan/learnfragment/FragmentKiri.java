2cdd515e2a5fc46ce42bd4ea0afcb14dd6dbbb2e
M Nur Fauzan W <fauzan.widyanto1@gmail.com>
2018-08-25 12:53:17 +0700

    belum fix

---
+++ app/src/main/java/widyanto/fauzan/learnfragment/FragmentKiri.java
@@ -0,0 +1,21 @@
+package widyanto.fauzan.learnfragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by M Nur Fauzan W on 25-Aug-18.
+ */
+
+public class FragmentKiri extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_kiri, container,false);
+    }
+}
--- app/src/main/java/widyanto/fauzan/learnfragment/MainActivity.java
+++ app/src/main/java/widyanto/fauzan/learnfragment/MainActivity.java
@@ -1,13 +1,47 @@
 package widyanto.fauzan.learnfragment;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.frameLayout)
+    FrameLayout frameLayout;
+    @BindView(R.id.btnFragment1)
+    Button btnFragment1;
+    @BindView(R.id.btnFragment2)
+    Button btnFragment2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+    }
+
+
+    @OnClick(R.id.btnFragment1)
+    public void onBtnFragment1Clicked() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.frameLayout, new FragmentKanan());
+        fragmentTransaction.commit();
+    }
+
+    @OnClick(R.id.btnFragment2)
+    public void onBtnFragment2Clicked() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.frameLayout, new FragmentKiri());
+        fragmentTransaction.commit();
     }
 }
